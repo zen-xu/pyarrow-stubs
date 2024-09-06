1b4b5f23a29deef9bec92a6bc9cc41106ab99ed6
ZhengYu, Xu <zen-xu@outlook.com>
2024-09-06 09:07:04 +0800

    fix: add missing submodule benchmark, csv and cuda (#71)

---
+++ pyarrow-stubs/benchmark.pyi
@@ -0,0 +1,3 @@
+from pyarrow.lib import benchmark_PandasObjectIsNull
+
+__all__ = ["benchmark_PandasObjectIsNull"]
---
+++ pyarrow-stubs/csv.pyi
@@ -0,0 +1,27 @@
+from pyarrow._csv import (
+    ISO8601,
+    ConvertOptions,
+    CSVStreamingReader,
+    CSVWriter,
+    InvalidRow,
+    ParseOptions,
+    ReadOptions,
+    WriteOptions,
+    open_csv,
+    read_csv,
+    write_csv,
+)
+
+__all__ = [
+    "ISO8601",
+    "ConvertOptions",
+    "CSVStreamingReader",
+    "CSVWriter",
+    "InvalidRow",
+    "ParseOptions",
+    "ReadOptions",
+    "WriteOptions",
+    "open_csv",
+    "read_csv",
+    "write_csv",
+]
