8907b748bdfe474787db4de8c93f0acde23786fc
ZhengYu, Xu <zen-xu@outlook.com>
2025-04-02 13:52:45 +0800

    Revert "Add Scalar, Array and Type classes for Json & Uuid (#194)"
    This reverts commit 8f779099262a3923f1ce2cef7b53770bda45441e.

--- pyarrow-stubs/__lib_pxi/array.pyi
+++ pyarrow-stubs/__lib_pxi/array.pyi
@@ -1587,9 +1587,6 @@ class ExtensionArray(Array[scalar.ExtensionScalar], Generic[_ArrayT]):
         typ: types.BaseExtensionType, storage: _ArrayT
     ) -> ExtensionArray[_ArrayT]: ...
 
-class JsonArray(ExtensionArray[_ArrayT]): ...
-class UuidArray(ExtensionArray[_ArrayT]): ...
-
 class FixedShapeTensorArray(ExtensionArray[_ArrayT]):
     def to_numpy_ndarray(self) -> np.ndarray: ...
     def to_tensor(self) -> Tensor: ...
@@ -1651,8 +1648,6 @@ __all__ = [
     "StructArray",
     "RunEndEncodedArray",
     "ExtensionArray",
-    "JsonArray",
-    "UuidArray",
     "FixedShapeTensorArray",
     "concat_arrays",
     "_empty_array",
