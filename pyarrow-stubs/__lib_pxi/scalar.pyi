6ac8250dbc524e26f484f2927916b5082fee2c7d
ZhengYu, Xu <zen-xu@outlook.com>
2025-06-17 11:13:31 +0800

    doc: complete scalar doc

--- pyarrow-stubs/__lib_pxi/scalar.pyi
+++ pyarrow-stubs/__lib_pxi/scalar.pyi
@@ -34,10 +34,19 @@ _AsPyTypeK = TypeVar("_AsPyTypeK")
 _AsPyTypeV = TypeVar("_AsPyTypeV")
 
 class Scalar(_Weakrefable, Generic[_DataTypeT]):
+    """
+    The base class for scalars.
+    """
     @property
-    def type(self) -> _DataTypeT: ...
+    def type(self) -> _DataTypeT:
+        """
+        Data type of the Scalar object.
+        """
     @property
-    def is_valid(self) -> bool: ...
+    def is_valid(self) -> bool:
+        """
+        Holds a valid (non-null) value.
+        """
     @overload
     def cast(
         self,
@@ -54,7 +63,43 @@ class Scalar(_Weakrefable, Generic[_DataTypeT]):
         options: CastOptions | None = None,
         memory_pool: MemoryPool | None = None,
     ) -> Scalar[_DataTypeT]: ...
-    def validate(self, *, full: bool = False) -> None: ...
+    def cast(self, *args, **kwargs):
+        """
+        Cast scalar value to another data type.
+
+        See :func:`pyarrow.compute.cast` for usage.
+
+        Parameters
+        ----------
+        target_type : DataType, default None
+            Type to cast scalar to.
+        safe : boolean, default True
+            Whether to check for conversion errors such as overflow.
+        options : CastOptions, default None
+            Additional checks pass by CastOptions
+        memory_pool : MemoryPool, optional
+            memory pool to use for allocations during function execution.
+
+        Returns
+        -------
+        scalar : A Scalar of the given target data type.
+        """
+    def validate(self, *, full: bool = False) -> None:
+        """
+        Perform validation checks.  An exception is raised if validation fails.
+
+        By default only cheap validation checks are run.  Pass `full=True`
+        for thorough validation checks (potentially O(n)).
+
+        Parameters
+        ----------
+        full : bool, default False
+            If True, run expensive checks, otherwise cheap checks only.
+
+        Raises
+        ------
+        ArrowInvalid
+        """
     def equals(self, other: Scalar) -> bool: ...
     def __hash__(self) -> int: ...
     @overload
@@ -125,6 +170,24 @@ class Scalar(_Weakrefable, Generic[_DataTypeT]):
         *,
         maps_as_pydicts: Literal["lossy", "strict"] | None = None,
     ) -> Any: ...
+    def as_py(self, *args, **kwargs):
+        """
+        Return this value as a Python representation.
+
+        Parameters
+        ----------
+        maps_as_pydicts : str, optional, default `None`
+            Valid values are `None`, 'lossy', or 'strict'.
+            The default behavior (`None`), is to convert Arrow Map arrays to
+            Python association lists (list-of-tuples) in the same order as the
+            Arrow Map, as in [(key1, value1), (key2, value2), ...].
+
+            If 'lossy' or 'strict', convert Arrow Map arrays to native Python dicts.
+
+            If 'lossy', whenever duplicate keys are detected, a warning will be printed.
+            The last seen value of a duplicate key will be in the Python dictionary.
+            If 'strict', this instead results in an exception being raised when detected.
+        """
 
 _NULL: TypeAlias = None
 NA = _NULL
@@ -276,7 +339,21 @@ class ExtensionScalar(Scalar[types.ExtensionType]):
     @property
     def value(self) -> Any | None: ...
     @staticmethod
-    def from_storage(typ: types.BaseExtensionType, value) -> ExtensionScalar: ...
+    def from_storage(typ: types.BaseExtensionType, value) -> ExtensionScalar:
+        """
+        Construct ExtensionScalar from type and storage value.
+
+        Parameters
+        ----------
+        typ : DataType
+            The extension type for the result scalar.
+        value : object
+            The storage value for the result scalar.
+
+        Returns
+        -------
+        ext_scalar : ExtensionScalar
+        """
 
 class Bool8Scalar(Scalar[types.Bool8Type]): ...
 class UuidScalar(Scalar[types.UuidType]): ...
@@ -284,8 +361,30 @@ class JsonScalar(Scalar[types.JsonType]): ...
 class OpaqueScalar(Scalar[types.OpaqueType]): ...
 
 class FixedShapeTensorScalar(ExtensionScalar):
-    def to_numpy(self) -> np.ndarray: ...
-    def to_tensor(self) -> Tensor: ...
+    def to_numpy(self) -> np.ndarray:
+        """
+        Convert fixed shape tensor scalar to a numpy.ndarray.
+
+        The resulting ndarray's shape matches the permuted shape of the
+        fixed shape tensor scalar.
+        The conversion is zero-copy.
+
+        Returns
+        -------
+        numpy.ndarray
+        """
+    def to_tensor(self) -> Tensor:
+        """
+        Convert fixed shape tensor extension scalar to a pyarrow.Tensor, using shape
+        and strides derived from corresponding FixedShapeTensorType.
+
+        The conversion is zero-copy.
+
+        Returns
+        -------
+        pyarrow.Tensor
+            Tensor represented stored in FixedShapeTensorScalar.
+        """
 
 _V = TypeVar("_V")
 
@@ -822,6 +921,45 @@ def scalar(
     from_pandas: bool | None = None,
     memory_pool: MemoryPool | None = None,
 ) -> Scalar[_DataTypeT]: ...
+def scalar(*args, **kwargs):
+    """
+    Create a pyarrow.Scalar instance from a Python object.
+
+    Parameters
+    ----------
+    value : Any
+        Python object coercible to arrow's type system.
+    type : pyarrow.DataType
+        Explicit type to attempt to coerce to, otherwise will be inferred from
+        the value.
+    from_pandas : bool, default None
+        Use pandas's semantics for inferring nulls from values in
+        ndarray-like data. Defaults to False if not passed explicitly by user,
+        or True if a pandas object is passed in.
+    memory_pool : pyarrow.MemoryPool, optional
+        If not passed, will allocate memory from the currently-set default
+        memory pool.
+
+    Returns
+    -------
+    scalar : pyarrow.Scalar
+
+    Examples
+    --------
+    >>> import pyarrow as pa
+
+    >>> pa.scalar(42)
+    <pyarrow.Int64Scalar: 42>
+
+    >>> pa.scalar("string")
+    <pyarrow.StringScalar: 'string'>
+
+    >>> pa.scalar([1, 2])
+    <pyarrow.ListScalar: [1, 2]>
+
+    >>> pa.scalar([1, 2], type=pa.list_(pa.int16()))
+    <pyarrow.ListScalar: [1, 2]>
+    """
 
 __all__ = [
     "Scalar",
